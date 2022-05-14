f88a8d450f2c9f88e7eaa86e2010058109a8cdc8
WinnieCWu <winniechn7@gmail.com>
2022-05-13 21:30:49 -0700

    set up components for portfolio

---
+++ winniecw-portfolio-via-react/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <Navigation />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Resume />
+      <main>
+        
+        <Project />
+
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
